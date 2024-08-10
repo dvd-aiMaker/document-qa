937849a7175e512496c5e158ed5b7527d81a7b79
dvd-aiMaker <anghelonedavid@gmx.com>
2024-08-10 12:08:05 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -118,7 +118,7 @@ def chat_multi_vision(image_paths, api_key, prompt):
         }
     ],
     "temperature": 0.1,
-    "top_p": 0.9999,
+    "top_p": 0.9,
     }
 
     response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
