06135e54100c00d222dabd83a609beff9ff9377c
dvd-aiMaker <anghelonedavid@gmx.com>
2024-08-10 11:03:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ else:
 
     if uploaded_file != None:
 
-        folder = "document-qa/content/data"
+        folder = "content/data"
         on_upload_change(uploaded_file,folder)
     
         image_paths = []
