be5fd88eb37de67effa9ce30de462a8ee9fa4fbb
dvd-aiMaker <anghelonedavid@gmx.com>
2024-08-10 11:15:22 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -48,11 +48,11 @@ def pdf2img(pdf_path,folder_path):
 def pdf_to_jpg(pdf, output_folder):
     # Creer le dossier automatique de sauvegarde
 
-    os.chmod("content", stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
-    os.chmod("content/data", stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
+    os.chmod("./content", stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
+    os.chmod("./content/data", stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
     os.chmod(output_folder, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
     
-    output_folder = "content/data"
+    #output_folder = "content/data"
     os.makedirs(output_folder, exist_ok=True)
 
     # Ouvrir le document PDF
