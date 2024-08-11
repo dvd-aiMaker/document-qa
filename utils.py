22c6179ef4be076c79df7a5a4eadd886309e6555
dvd-aiMaker <anghelonedavid@gmx.com>
2024-08-11 16:28:51 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -140,7 +140,7 @@ def chat_df(image_paths, api_key, prompt1):
     response1 = chat_multi_vision(image_paths, api_key, prompt1)
 
     table1 = response1.json()["choices"][0]['message']['content']
-    #st.text(str(table1))
+    st.text(str(table1))
     
     table1json = table1.split("```json")[1].split("```")[0]
     table1json = table1.split("```json")[1].split("```")[0]
