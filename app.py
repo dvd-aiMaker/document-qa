b99f42a62740b3ea458d1c98aef3ce0b283bcb47
dvd-aiMaker <anghelonedavid@gmx.com>
2024-08-11 16:06:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -171,7 +171,15 @@ if st.session_state.get("logged_in"):
                 df, df_show = extract_text_from_invoice(image_paths ,openai_api_key,selection)
                 st.dataframe(df)
                 st.dataframe(df_show)
-            
+
+                st.markdown("**Resultat de l'Analyse**")
+                if selection == "Ponctuel":
+                    st.text("Valeur Totale: "+ str(df_show['Montant'].sum()))
+                    st.text("Poids Total: "+ str(df_show["Poids_total"].sum()))
+                elif selection == "Grosfillex":
+                    st.text("Valeur Totale: "+ str(df_show['Valeur'].sum()))
+                    st.text("Poids Total: "+ str(df_show["Poids"].sum()))
+                
 
             
             #df, df_show = extract_text_from_invoice(image_paths,openai_api_key,selection)
