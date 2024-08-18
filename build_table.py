bd546b77623ffa93293259a856a73359fbd9a74c
dvd-aiMaker <anghelonedavid@gmx.com>
2024-08-18 17:16:42 +0200

    ajout de Levac

--- build_table.py
+++ build_table.py
@@ -58,6 +58,17 @@ def process_df(df, selection):
     df['Valeur'] = pd.to_numeric(df['Valeur'], errors='coerce')
     df["Poids"] = pd.to_numeric(df['Poids'], errors='coerce')
 
+elif selection == "Levac":
+    if "Valeur_Douane" not in df.columns and "Valeur Douane" in df.columns:
+      df.rename(columns={"Valeur Douane": "Valeur_Douane"}, inplace=True)
+
+    df['Poids_total'] = df['Poids'] * df['Quantités']
+    # Création de la nouvelle colonne Valeur_totale
+    df['Valeur_totale'] = df.apply(lambda row: row['Valeur'] * row['Quantités'] if row['Valeur'] != 0 else row['Valeur_Douane'] * row['Quantités'], axis=1)
+    
+    df['Valeur'] = pd.to_numeric(df['Valeur'], errors='coerce')
+    df["Poids_total"] = pd.to_numeric(df['Poids_total'], errors='coerce')
+
     
   else:
     print("Le client n'est pas dans la base")
@@ -68,6 +79,8 @@ def compute_df(df, selection):
     return compute_df_Ponctuel(df)
   elif selection == "Grosfillex":
     return compute_df_Grosfillex(df)
+  elif selection == "Levac":
+    return compute_df_Levac(df)
   else:
     print(selection + "is Not ready")
 
@@ -149,6 +162,54 @@ def compute_df_Grosfillex(df):
   # Concaténation des deux DataFrames
   df_combined = pd.concat([df_eu_aggregated, df_non_eu_aggregated], ignore_index=True)
   return df_combined
+
+
+def compute_df_Levac(df):
+    # Création de la DataFrame pour les origines EU
+    df_eu = df[df['Origine'].isin(eu_country_codes)]
+
+    # Agrégation des données par code douanier pour les origines EU
+    df_eu_aggregated = df_eu.groupby('Code_Douane').agg({
+    'ID': lambda x: list(x),
+    'Valeur': 'sum',
+    'Valeur_Douane': 'sum',
+    'Poids': 'sum',
+    'Quantités': 'sum',
+    'Poids_total': 'sum',
+    'Valeur_totale': 'sum',
+    'Montant': 'sum'
+    }).reset_index()
+    df_eu_aggregated["ORIGINE"] = "EU"
+
+    # Renommer la colonne ID en ID_agrégés
+    df_eu_aggregated.rename(columns={'ID': 'ID_agrégés'}, inplace=True)
+
+
+    # Création de la DataFrame pour les origines hors EU
+    df_non_eu = df[~df['Origine'].isin(eu_country_codes)]
+
+
+    # Agrégation des données par code douanier pour les origines hors EU
+    df_non_eu_aggregated = df_non_eu.groupby('Code_Douane').agg({
+    'ID': lambda x: list(x),
+    'Valeur': 'sum',
+    'Valeur_Douane': 'sum',
+    'Poids': 'sum',
+    'Quantités': 'sum',
+    'Poids_total': 'sum',
+    'Valeur_totale': 'sum',
+    'Montant': 'sum'
+    }).reset_index()
+    df_non_eu_aggregated["ORIGINE"] = "HORS EU"
+
+    # Renommer la colonne ID en ID_agrégés
+    df_non_eu_aggregated.rename(columns={'ID': 'ID_agrégés'}, inplace=True)
+
+
+    # Concaténation des deux DataFrames
+    df_combined = pd.concat([df_eu_aggregated, df_non_eu_aggregated], ignore_index=True)
+    return df_combined
+
 
 def extract_text_from_invoice(image_paths, api_key, selection):
   df = chat_df(image_paths, api_key, GPT_prompt(selection))
