a71a5606385ae813719e3caa87173de0abb8e350
dvd-aiMaker <anghelonedavid@gmx.com>
2024-08-20 20:44:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -174,7 +174,7 @@ if st.session_state.get("logged_in"):
         st.write(f"Téléchargez la facture du client {selection}")
         
         # Charger un fichier PDF si nécessaire
-        uploaded_file = st.file_uploader("Téléchargez la facture comme fichier PDF", type="pdf", accept_multiple_files=True)
+        uploaded_file = st.file_uploader("Téléchargez la facture comme fichier PDF", type="pdf")
         mark = 0
 
 
